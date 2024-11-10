6797cede46cd978f3d5ec40480b8458cf5856af8
Ray Chen <rc144@outlook.com>
2024-11-09 21:24:38 -0500

    add numeric textbox

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,47 @@
 import $ from 'jquery';
 
 
+const NUMERALS = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+
 $(() => {
+    $(".numeric")[0].addEventListener("keydown", evalNumeric);
+
     $(".persist").each(function () {
-        const isCheckbox = (<HTMLInputElement>this).type === "checkbox"
+        const isCheckbox = (<HTMLInputElement>this).type === "checkbox";
 
-            chrome.storage.local.get([this.id]).then((result) => {
-            if(typeof result[this.id] === "undefined") {
-                if(isCheckbox) {
-                    (<HTMLInputElement>this).checked = false;
-                } else {
-                    (<HTMLInputElement>this).value = "";
-                }
+        chrome.storage.local.get([this.id]).then((result) => {
+        if(typeof result[this.id] === "undefined") {
+            if(isCheckbox) {
+                (<HTMLInputElement>this).checked = false;
             } else {
-                if(isCheckbox) {
-                    (<HTMLInputElement>this).checked = result[this.id]
-                } else {
-                    (<HTMLInputElement>this).value = result[this.id];
-                }
+                (<HTMLInputElement>this).value = "";
             }
-        }, (_) => {
-            (<HTMLInputElement>this).value = "";
-        });
-
-        if(isCheckbox) {
-            this.addEventListener("change", cPersist);
         } else {
-            this.addEventListener("input", storePersist);
+            if(isCheckbox) {
+                (<HTMLInputElement>this).checked = result[this.id];
+            } else {
+                (<HTMLInputElement>this).value = result[this.id];
+            }
         }
+    }, (_) => {
+        (<HTMLInputElement>this).value = "";
+    });
+
+    if(isCheckbox) {
+        this.addEventListener("change", cPersist);
+    } else {
+        this.addEventListener("input", storePersist);
+    }
     });
 });
+
+function evalNumeric(this: HTMLElement, event: KeyboardEvent) {
+    if(!NUMERALS.has(event.key) && event.key !== "Backspace")
+    {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+}
 
 function storePersist(this: HTMLInputElement) {
     chrome.storage.local.set({ [this.id]: this.value }).then();
