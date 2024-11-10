9f98e72aa4915e67ec79a31d4ff940a269690e01
mackenzieewong <mackenzie.wong@uwaterloo.ca>
2024-11-10 02:32:17 -0500

    dino dialogue

--- src/dinoOverlay.ts
+++ src/dinoOverlay.ts
@@ -4,22 +4,44 @@ const normalDino = "https://i.imgur.com/40oGV3K.png";
 const angryDino = "https://i.imgur.com/uXZtDEm.png";
 const angrierDino = "https://i.imgur.com/Yv0oae2.png";
 
+const dialogue = "RAHHHHHHHH"; // hardcode dialogue for now
+const speechBubble = "https://i.imgur.com/G0hFWOJ.png"; 
+
 const dinoImage = normalDino; // hardcode default to not angry dino 
 
 export const addDinoOverlay = () => {
-    // Create the dino image element
-    const dinoWrapper = document.createElement("div");
+    const container = document.createElement("div");
+    container.classList.add("container");
 
+    const dinoContainer = document.createElement("div");
+    dinoContainer.classList.add("dino-container");
 
+    const textContainer = document.createElement("div");
+    textContainer.classList.add("text-container");
+
+    // Text box element
+    const dinoText = document.createElement("div");
+    dinoText.innerText = dialogue;
+    dinoText.classList.add("dino-text");
+
+    // Append the text to the text container
+    textContainer.appendChild(dinoText);
+    // Append the text container to the main container 
+    container.appendChild(textContainer);
+
+    // Dino image element 
     const dinoImageOverlay = document.createElement("img");
     dinoImageOverlay.classList.add("dino");
     dinoImageOverlay.src = dinoImage; // Pass the image path as a parameter
 
-    // Append image to wrapper
-    dinoWrapper.appendChild(dinoImageOverlay);
+    // Append dino image to container
+    dinoContainer.appendChild(dinoImageOverlay);
+    // Append dino container to main container 
+    container.appendChild(dinoContainer);
 
-    // Add wrapper to the body of the page
-    document.body.appendChild(dinoImageOverlay);
+    // Add container to the body of the page
+    document.body.appendChild(container);
 
-    console.log("Dino added to page");
+    console.log("Dino with text added to page");
   };
+
